09c3af64fa4584bf5b90d72de2923142e12c96c2
Saisantoshkumar Tuniki <saisantosh.tuniki@onpassive.com>
2024-01-18 10:07:17 +0530

    Reeborg Hurdle 4 added

---
+++ Reeborg/Hurdle 4.py
@@ -0,0 +1,28 @@
+def turn_right():
+    turn_left()
+    turn_left()
+    turn_left()
+
+
+def jump():
+    turn_left()
+    while wall_on_right():
+        move()
+    turn_right()
+    move()
+    turn_right()
+    while not wall_in_front():
+        move()
+    turn_left()
+
+
+while not at_goal():
+    if (wall_in_front()):
+        jump()
+    else:
+        move()
+
+################################################################
+# WARNING: Do not change this comment.
+# Library Code is below.
+################################################################
