32976b8d98371555f221646c5d910f9079606a07
Saisantoshkumar Tuniki <saisantosh.tuniki@onpassive.com>
2024-01-24 10:06:06 +0530

    caesar cipher done

--- Day_08/caesar_cipher.py
+++ Day_08/caesar_cipher.py
@@ -1,32 +1,46 @@
+from art import logo
+
 alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l',
             'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
 
+def caesar_cipher():
+    direction = input(
+        "Type 'encode' to encrypt, type 'decode' to decrypt:\n"
+    ).lower()
+    text = input("Type your message:\n").lower()
+    shift = int(input("Type the shift number:\n"))
 
-direction = input(
-    "Type 'encode' to encrypt, type 'decode' to decrypt:\n"
-).lower()
-text = input("Type your message:\n").lower()
-shift = int(input("Type the shift number:\n"))
+    length = len(alphabet)
+    shift = shift % length
 
-length = len(alphabet)
+    def caesar(text, shift, direction):
+        result = ''
+        for char in text:
+            if char in alphabet:
+                position = alphabet.index(char)
+                new_position = 0
+                if direction == 'encode':
+                    new_position = position+shift
+                    if new_position > (length-1):
+                        new_position = new_position - length
+                else:
+                    new_position = position - shift
+                result += alphabet[new_position]
+            else:
+                result += char
+        print(f"The {direction}d text is {result}")
+
+    if direction == 'encode' or direction == 'decode':
+        caesar(text, shift, direction)
+    else:
+        print('You have entered unknown text...')
+
+    choice = input("Type 'yes' if you want to go again. Otherwise type 'no'.\n").lower()
+    if choice == 'yes':
+        caesar_cipher()
+    else:
+        print('Goodbye!')
 
 
-def caesar(text, shift, direction):
-    result = ''
-    for letter in text:
-        position = alphabet.index(letter)
-        new_position = 0
-        if direction == 'encode':
-            new_position = position+shift
-            if new_position > (length-1):
-                new_position = new_position - length
-        else:
-            new_position = position - shift
-        result += alphabet[new_position]
-    print(f"The {direction}d text is {result}")
-
-
-if direction == 'encode' or direction == 'decode':
-    caesar(text, shift, direction)
-else:
-    print('You have entered unknown text...')
+print(logo)
+caesar_cipher()
