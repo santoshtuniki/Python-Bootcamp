07d38fd684ea68f3c0accb3ea4a7647445d6794b
Saisantoshkumar Tuniki <saisantosh.tuniki@onpassive.com>
2024-01-30 09:56:02 +0530

    Calculatoe added

---
+++ Day_10/calculator.py
@@ -0,0 +1,40 @@
+from art import logo
+
+
+def add(n1, n2):
+    return n1+n2
+
+
+def subtract(n1, n2):
+    return n1-n2
+
+
+def multiply(n1, n2):
+    return n1*n2
+
+
+def divide(n1, n2):
+    return n1/n2
+
+
+operations = {
+    "+": add,
+    "-": subtract,
+    "*": multiply,
+    "/": divide
+}
+
+
+num1 = int(input("What is your first number?\n"))
+num2 = int(input("What is your second number?\n"))
+
+for symbol in operations:
+    print(symbol)
+
+operation_symbol = int(input("Pick an operation symbol from the line above: "))
+
+function = operations[operation_symbol]
+
+answer = function(num1, num2)
+
+print(f"{num1} {operation_symbol} {num2} = {answer}")
