70876f9bc5674084bfa16a13f60df7ef26672c09
Saisantoshkumar Tuniki <saisantosh.tuniki@onpassive.com>
2024-03-01 09:46:51 +0530

    High Vs Low done

--- Projects/14_High_Low.py/main.py
+++ Projects/14_High_Low.py/main.py
@@ -4,6 +4,7 @@ from art import logo, vs
 from game_data import data
 
 game_over = False
+score = 0
 
 
 def clear():
@@ -23,8 +24,44 @@ def indexOfB(indexA):
         return index
 
 
+def description(index):
+    '''Returns the string describing the comparison'''
+    data_selected = data[index]
+    return (f"{data_selected['name']}, a {data_selected['description']}, from {data_selected['country']}")
+
+
+def compare(indexA, indexB):
+    '''Returns the one with the highest follower count'''
+    dataA = data[indexA]
+    dataB = data[indexB]
+
+    if dataA["follower_count"] > dataB["follower_count"]:
+        return 'a'
+    else:
+        return 'b'
+
+
 indexA = randomIndex()
 
-indexB = indexOfB(indexA)
 
+while not game_over:
+    clear()
+    print(logo)
 
+    print(f"Compare A: {description(indexA)}")
+    print(vs)
+
+    indexB = indexOfB(indexA)
+    print(f"Compare B: {description(indexB)}")
+
+    user_choice = input("Who has more followers? Type 'A' or 'B': ").lower()
+    result = compare(indexA, indexB)
+    
+    if user_choice == result:
+        score += 1
+        indexA = indexB
+    else:
+        clear()
+        print(logo)
+        print(f"Sorry, that's wrong. Final score: {score}")
+        game_over = True
